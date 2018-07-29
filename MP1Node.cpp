1ec144a7eae2c704e83479180d690a50f79a4877
Vara Varavithya <varavithya@gmail.com>
2018-07-29 11:24:13 +0700

    I added the print out for Send reply messge at the start.
    Vara

--- MP1Node.cpp
+++ MP1Node.cpp
@@ -198,12 +198,27 @@ void MP1Node::nodeLoop() {
 void MP1Node::checkMessages() {
     void *ptr;
     int size;
-
+    MessageHdr *msg;
+    
     // Pop waiting messages from memberNode's mp1q
     while ( !memberNode->mp1q.empty() ) {
     	ptr = memberNode->mp1q.front().elt;
     	size = memberNode->mp1q.front().size;
     	memberNode->mp1q.pop();
+	
+	
+#ifdef DEBUGLOG
+	cout << "------------------------------------\n";
+	cout << "checkMessage() This Node\t" <<  memberNode->addr.getAddress() << "\t";;
+	cout << "Time:" << par->getcurrtime() << endl;  
+	msg =  (MessageHdr *) ptr;
+	cout << "MessageType\t" << msg->msgType << endl;
+	cout << "Message Size\t" << size << endl;
+	cout << "Source address\t";
+	printAddress((Address *) (msg+1));
+	cout << "\n------------------------------------\n";
+#endif
+	
     	recvCallBack((void *)memberNode, (char *)ptr, size);
     }
     return;
@@ -218,6 +233,59 @@ bool MP1Node::recvCallBack(void *env, char *data, int size ) {
 	/*
 	 * Your code goes here
 	 */
+  MessageHdr *msg, *recvMsg;
+
+#ifdef DEBUGLOG
+  static char s[1024];
+#endif
+  
+  Address *desaddr;
+
+  
+  desaddr = (Address *) ((MessageHdr *) data+1); // Big Endian Little Endian
+    
+  printAddress(desaddr);
+
+#ifdef DEBUGLOG
+  cout << "------------------------------------\n";
+  cout << "recvCallBack() at time: " << par->getcurrtime() << " This node: ";
+  cout << memberNode->addr.getAddress() << " From node: ";
+  printAddress((Address *) desaddr);
+  cout << "------------------------------------\n";
+#endif
+  
+  size_t msgsize = sizeof(MessageHdr) + sizeof(memberNode->addr.addr) + sizeof(long) + 1;
+  msg = (MessageHdr *) malloc(msgsize * sizeof(char));
+
+  msg->msgType = JOINREP;
+  memcpy((char *)(msg+1), &memberNode->addr.addr, sizeof(memberNode->addr.addr));
+  memcpy((char *)(msg+1) + 1 + sizeof(memberNode->addr.addr), &memberNode->heartbeat, sizeof(long));
+
+#ifdef DEBUGLOG
+  cout << "Member Node Address (in msg) ";
+  printAddress((Address *) (msg+1));
+  cout << "Heartbeat (in msg)";
+  printf("%d\n", *((long *) (msg+1+sizeof(memberNode->addr.addr))));
+  sprintf(s, "Send JOINREP to %d.%d.%d.%d:%d", desaddr->addr[0], desaddr->addr[1], desaddr->addr[2], desaddr->addr[3], *(short*)&desaddr->addr[4]);   
+  cout << "------------------------------------\n";
+  cout << "Send Join Reply at Time " << par->getcurrtime() << " From This node: ";
+  cout << memberNode->addr.getAddress() << " To node: ";
+  printAddress((Address *) desaddr);
+  cout << "Member Node Address ";
+  printAddress(&memberNode->addr);
+  cout << "Destination Address ";
+  printAddress(desaddr);
+#endif
+  
+  log->LOG(&memberNode->addr, s);
+  
+  // send JOINREP message to introducer member
+  emulNet->ENsend(&memberNode->addr, desaddr, (char *)msg, msgsize);
+  
+  free(msg);
+
+  return 1;
+  
 }
 
 /**
